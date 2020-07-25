6b1547ceef6ef7be7cda8ef22c2308b79de57a65
Alex Cuenca M <maxpowerxd8y9@gmail.com>
2020-07-24 21:26:56 -0500

    Nuevos cambios

---
+++ Estudiantes/Estudiante.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudiantes
+{
+    class Estudiante
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Ci { get; set; }
+        public string Telefono { get; set; }
+
+        public Estudiante(string nombre,string apellido, string ci, string telefono)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Ci = ci;
+            Telefono = telefono;
+        }
+
+        public override string ToString()
+        {
+            return $"Nombre: {Nombre}\nApellido: {Apellido}\nCI: {Ci}\nTeléfono: {Telefono}";
+        }
+    }
+}

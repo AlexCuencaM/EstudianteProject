2afea853c28d854c305c3e9bfc195e22c841826f
Alex Cuenca M <maxpowerxd8y9@gmail.com>
2020-08-12 18:18:30 -0500

    Lesson

--- Estudiantes/Program.cs
+++ Estudiantes/Program.cs
@@ -4,49 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Estudiantes
+namespace Clientes
 {
     class Program
     {
-        static List<Estudiante> clase = new List<Estudiante>();
-        static Promedio promedio;
+        static Empleado e;
+        static Cliente c;       
+        static Compras promedio;
         static string input(string label)
         {
             Console.WriteLine($"Ingrese {label}: ");
             return Console.ReadLine();
         }
-
+        static List<double> compras = new List<double>();
         static void textMenu()
         {
             Console.WriteLine("Menu Principal");
-            Console.WriteLine("1.-Ingresar Estudiante");            
-            Console.WriteLine("5.-Ingresar 4 notas");                        
-            Console.WriteLine("6.-Ver reporte");
-            Console.WriteLine("7.-Salir");
+            Console.WriteLine("1.-Ingresar clientes");
+            Console.WriteLine("2.-Ingresar Empleado");
+            Console.WriteLine("3.-Ingresar Compra");                        
+            Console.WriteLine("4.-Ver reporte");
+            Console.WriteLine("5.-Salir");
             Console.WriteLine("Ingrese una Opcion: ");
         }
-        static void ingresar(List<Estudiante> clase)
+
+        static void ingresar(out Cliente c)
         {            
             //Como List es un objeto el parametro es por referencia :D
-            Console.WriteLine("Ingrese los datos del estudiante:\n");
-            clase.Add(new Estudiante(input("nombre"),
-                input("apellido"), input("CI"), input("telefono")));
+            Console.WriteLine("Ingrese los datos del Cliente:\n");
+            c = new Cliente(input("nombre"),
+                input("apellido"), input("CI"), input("telefono"));
             finalOpcion();
-        }        
-        static void IngresarNotas(Estudiante e, out Promedio promedio)
+        }     
+        static void ingresarEmpleado(out Empleado empleado)
         {
-            
-            List<double> notas = new List<double>();
-            for(int i = 0; i < 4; i++)                          
-                notas.Add(Double.Parse(input($"Nota {i}")));            
-            promedio = new Promedio(e, notas);
+            Console.WriteLine("Ingrese los datos del Empleado:\n");
+            empleado = new Empleado(input("nombre"),
+                input("apellido"), input("CI"), input("telefono"));
             finalOpcion();
         }
-        static void reporte(Promedio p)
+        static void IngresarCompra(Cliente e)
+        {                                                
+            compras.Add(Double.Parse(input("Compra")));
+            finalOpcion();
+        }
+        static void reporte(out Compras p)
         {
-            PromedioAltoBajo prom = new PromedioAltoBajo(p.Estudiante,p.Notas);
-            Console.WriteLine($"Nombre de alumno: {prom.Estudiante.Nombre}\nNota Más baja: {prom.notaMasBaja()}\nPromedio original: {p.promedio()}\n" +
-                $"Promedio sin la nota mas baja: {prom.promedio()}\nCondición del alumno: {prom.condicionEstudiante()}");
+            p = new Compras(c,e, compras);
+            Console.WriteLine(p);
             finalOpcion();
         }
         private static void finalOpcion()
@@ -63,15 +68,21 @@ namespace Estudiantes
             switch (switch_on)
             {                
                 case 1:
-                    ingresar(clase);
-                    break;                
-                case 5:                    
-                    IngresarNotas(clase.FirstOrDefault(),out promedio);
+                    ingresar(out c);
                     break;
-                case 6:
-                    reporte(promedio);
+                case 2:
+                    ingresarEmpleado(out e);
                     break;
-                case 7:
+                case 3:                    
+                    IngresarCompra(c);
+                    break;
+                case 4:
+                    reporte(out promedio);
+                    break;
+                case 5:
+                    break;
+                default:
+                    Console.WriteLine("Ingrese un numero valido");
                     break;
             }
         }        
